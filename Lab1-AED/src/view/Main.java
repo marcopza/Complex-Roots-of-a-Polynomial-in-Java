a618d7ccd1597f26f3484ebebb91477c39c3bfb5
Jose <joseossorio99@hotmail.com>
2019-02-16 09:45:18 -0500

    created Main class and MainWindow.fxml

---
+++ Lab1-AED/src/view/Main.java
@@ -0,0 +1,32 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/view/MainWindow.fxml"));
+		Parent root = loader.load();
+		Scene mainScene = new Scene(root);
+		primaryStage.setScene(mainScene);
+		primaryStage.show();
+		
+	}
+	
+	@Override
+	public void stop() {
+		System.exit(0);
+	}
+	
+}
