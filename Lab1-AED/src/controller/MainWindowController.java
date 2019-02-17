95355ec02b97e93443ed5669456c82666597b7c3
Johan Camilo <camilo4182@gmail.com>
2019-02-17 03:32:23 -0500

    user interface.

--- Lab1-AED/src/controller/MainWindowController.java
+++ Lab1-AED/src/controller/MainWindowController.java
@@ -1,5 +1,50 @@
 package controller;
 
-public class MainWindowController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class MainWindowController implements Initializable {
+
+    @FXML private TextField c10TextField;
+    @FXML private TextField c9TextField;
+    @FXML private TextField c8TextField;
+    @FXML private TextField c7TextField;
+    @FXML private TextField c6TextField;
+    @FXML private TextField c5TextField;
+    @FXML private TextField c4TextField;
+    @FXML private TextField c3TextField;
+    @FXML private TextField c2TextField;
+    @FXML private TextField c1TextField;
+    @FXML private TextField c0TextField;
+    @FXML private Button randomPolMethod;
+    @FXML private Button newtonMethodButton;
+    @FXML private Button secantMethodButton;
+    @FXML private TextField rootsTextField;
+
+    @Override
+	public void initialize(URL location, ResourceBundle resources) {		
+		
+	}
+
+	@FXML
+    void newtonMethod(ActionEvent event) {
+
+    }
+
+    @FXML
+    void randomPolynomial(ActionEvent event) {
+
+    }
+
+    @FXML
+    void secantMethod(ActionEvent event) {
+
+    }
 
 }
