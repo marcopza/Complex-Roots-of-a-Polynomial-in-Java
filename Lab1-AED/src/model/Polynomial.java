40c264f26cd1a07da2375cce8907e72dd0d85b0a
Marco Perez <marcopza99@hotmail.com>
2019-02-16 23:47:58 -0500

    Ideas on how to structure the code

---
+++ Lab1-AED/src/model/Polynomial.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Polynomial {
+
+	private int orderOfGrowth; 
+	//Podemos hacerlo como si fuese una lista enlazada o usar un arrayList. 
+	//private String expression;
+	//private ArrayList<String> ExpersiónPostSplit
+	// ó
+	//private Expression primera;
+	
+	public ArrayList<Integer> newtonMethod(){
+		
+		return new ArrayList<Integer>();
+		
+	}
+	
+	public ArrayList<Integer> secantMethod(){
+		
+		return new ArrayList<Integer>();
+		
+	}
+	
+}
