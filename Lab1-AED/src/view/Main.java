b66c93ccbae3efd45c90602903a082913713b2bd
Marco Perez <marcopza99@hotmail.com>
2019-02-17 15:16:15 -0500

    Added the COmplexNumber Class

--- Lab1-AED/src/view/Main.java
+++ Lab1-AED/src/view/Main.java
@@ -6,10 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application{
 
 	public static void main(String[] args) {
 		launch();
+		
 	}
 
 	@Override
