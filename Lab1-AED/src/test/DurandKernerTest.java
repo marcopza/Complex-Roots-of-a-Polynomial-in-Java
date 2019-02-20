897da5163cd03883ab7df13a70feae0d7f7b399f
Marco Perez <marcopza99@hotmail.com>
2019-02-20 16:33:41 -0500

    Tets not working tho

---
+++ Lab1-AED/src/test/DurandKernerTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import model.ComplexPolynomial;
+import org.jscience.mathematics.number.Complex;
+
+class DurandKernerTest {
+
+	ComplexPolynomial poly;
+	ArrayList<Complex> expected;
+	
+	void setUpScenario1() {
+//		double[] coeff = new double[];
+//		poly = new ComplexPolynomial();
+	}
+	
+	@Test
+	void testRoots() {
+		fail("Not yet implemented");
+	}
+
+}
